33d5227b3dcab14459546c3c8685472e6829e621
Matevos Karapetyan <matevos@espoc.com>
2021-03-29 12:58:24 +0400

    Created Leverage Async Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,22 +7,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductionConfigService {}
-
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    {
-      provide: ConfigService,
-      useClass:
-        process.env.NODE_ENV === 'development'
-          ? DevelopmentConfigService
-          : ProductionConfigService,
-    },
     {
       provide: COFFEE_BRANDS, // 👈
       useValue: ['buddy brew', 'nescafe'], // array of coffee brands,
